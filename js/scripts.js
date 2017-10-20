48ec98bf1b25525fbd1dae5db94add6f5007b316
Chris Johnson <cknightjohnson@gmail.com>
2017-10-20 10:19:39 -0700

    add branching to handle displaying results.

--- js/scripts.js
+++ js/scripts.js
@@ -32,5 +32,28 @@ $('document').ready(function(){
     console.log('php ' + phpScore);
     console.log('ruby ' + rubyScore);
 
+    var highestScore = 0;
+    highestScore = Math.max(cSharpScore, javaScore, phpScore, rubyScore);
+    var winnerArr = [];
+    if (highestScore === cSharpScore) {
+      winnerArr.push('C#')
+    }
+    if (highestScore === javaScore) {
+      winnerArr.push('Java')
+    }
+    if (highestScore === phpScore) {
+      winnerArr.push('PHP')
+    }
+    if (highestScore === rubyScore) {
+      winnerArr.push('Ruby')
+    }
+
+    $('#resultsDiv').show();
+    if (winnerArr.length > 1){
+      $('#resultsDiv').text('You seem to like ' + winnerArr[0] + ' and ' + winnerArr[1] + 'the most. You should check those things out!');
+    } else {
+      $('#resultsDiv').text('Looks like your definitely a ' + winnerArr[0] + 'person. Sign up today!')
+    }
+
   });
 });
